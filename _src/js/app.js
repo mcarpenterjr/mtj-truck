8bd4928c810e4e5d639edd0474459454bd24a8d6
Mark Carpenter Jr <marksthings24@gmail.com>
2016-03-27 22:33:32 -0400

    bugfix: Social Media

--- _src/js/app.js
+++ _src/js/app.js
@@ -27,12 +27,12 @@ $(document).ready(function() {
 
   /* Face Book JS */
   $.ajaxSetup({ cache: true });
-    $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+    $.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
       FB.init({
         appId: '{1078294885527262}',
         version: 'v2.5' // or v2.0, v2.1, v2.2, v2.3
       });
       $('#loginbutton,#feedbutton').removeAttr('disabled');
-      FB.getLoginStatus(updateStatusCallback);
+      //FB.getLoginStatus(updateStatusCallback);
     });
 });
