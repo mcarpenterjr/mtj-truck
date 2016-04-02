5345fe0d83b41c8919b6df7d4013af117e7f95f9
Mark Carpenter Jr <marksthings24@gmail.com>
2016-04-02 16:47:00 -0400

    refactor: restructured app

---
+++ _src/assets/js/app.js
@@ -0,0 +1,79 @@
+var appInit = function() {
+  var self = this;
+
+  function dispMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 17,
+      mapTypeId: google.maps.MapTypeId.HYBRID,
+      center: {
+        lat: 41.176,
+        lng: -73.1615,
+      },
+      disableDefaultUI: true
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+
+    service.getDetails({
+      placeId: 'ChIJz88P1scN6IkRW_KuFCMb-uY'
+    }, function(place, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+            'Place ID: ' + place.place_id + '<br>' +
+            place.formatted_address + '</div>');
+          infowindow.open(map, this);
+        });
+      }
+    });
+  }
+
+  // Fires Up the Map.
+  self.map = dispMap();
+
+
+  $(document).ready(function() {
+    $('.parallax').parallax();
+    $(".button-collapse").sideNav();
+
+    /* Face Book JS */
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId: '1078294885527262',
+        xfbml: true,
+        version: 'v2.5'
+      });
+    };
+
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  });
+};
+
+var loadError = function() {
+  Materialize.toast('Trouble Getting Directions.', 5000, 'rounded');
+};
+
+/* TODO: Create get directions function using geo location*/
+/* Start with this html --> */
+/* use a swal modal to launch directions in another tab. */
+
+//   <form action="http://maps.google.com/maps" method="get" target="_blank">
+//    <label for="saddr">Enter your location</label>
+//    <input type="text" name="saddr" />
+//    <input type="hidden" name="daddr" value="350 5th Ave New York, NY 10018 (Empire State Building)" />
+//    <input type="submit" value="Get directions" />
+// </form>
--- _src/assets/js/data.js
+++ _src/assets/js/data.js
@@ -5,10 +5,10 @@ function model() {
     name: 'M. T. J Truck Equipment INC.',
     shortName: 'M. T. J.',
     contacts: {
-      phone: '(203)816-5206',
-      email: 'marksthings24@gmail.com',
+      phone: '(203)334-4939',
+      email: 'sales@mtjtruck.com',
       googlePlus: 'xXSirenSxOpusXx',
-      faceBook: 'Mark N Carpenter Jr',
+      faceBook: 'MTJTruckEquipmentINCBridgeport',
       address: {
         street: '127 Wilmot Ave.',
         state: 'Connecticut',
@@ -16,32 +16,23 @@ function model() {
       },
     },
     mainPic: 'img/profile/badge.jpg',
-    short: 'With strong problem solving skills, the ability to adapt,' +
-      ' and capability to quickly learn new tasks, I know I will become a vital' +
-      ' asset to almost any team. ',
-    services: ['HTML', 'CSS', 'JavaScript', 'Jquery', 'Bootstrap', 'CMS', 'Netbeans',
-      'Git', 'GitHub', 'MySql', 'iOS', 'Windows', 'CLI', 'PhotoShop', 'Fireworks',
-      'Premier Pro', 'Illustrator', 'DreamWeaver', 'Joomla', 'WordPress', 'MVC',
-      'CodeIgniter',
-    ],
+    short: 'Proffesinally installed truck equipment',
+    services: ['some array of traits'],
   };
 
   self.projects = [{
-    employer: 'G. P. Tool Co.',
-    type: 'CNC Machinist',
     size: {
       length: null,
       width: null,
       height: null,
     },
-    location: 'Danbury, CT',
     description: 'Producing machined parts for the Aerospace, Medical, ' +
       'and Energy Generation industries. Works from supplied blueprints and ' +
       'drawings to create excellent qaulity parts per customer specifications. ' +
       'Keeps a clean and safe work area along with routine equipment maintinence ' +
       'and quality checks. Works closely with Quality Control to meet Zero-Loss ' +
       'Certified Material requirements.',
-    duties: ['Studies Blueprints', 'Verifies Program Through Measurements ' +
+    features: ['Studies Blueprints', 'Verifies Program Through Measurements ' +
       'and Feedback From Quality Control', 'Maintains Production Consistency Through ' +
       'Attention to Part Details', 'Maintains a Safe Work Enviroment', 'Maintians Quality ' +
       'and Production Logs', 'Explores Oppurtunities to Add Value to Jobs',
